1faf3365b6f94fecb8258d26db94f261897b6e70
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-09-12 13:47:28 +0100

    created routes folder and made route file

--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const successStatus = 200;
 
-router.route("/").get(req, res => {
-    res.status(200).json({
-        message: "All contacts"
-    });
+router.route("/").get((req, res) => {
+    res.status(successStatus).json({message: "All contacts" });
 });
+
+router.route("/:id").get((req, res) => {
+    res.status(successStatus).json({message: `Get contact for ${req.param.id}` });
+});
+
+router.route("/").post((req, res) => {
+    res.status(successStatus).json({message: `Create contact` });
+});
+
+router.route("/:id").put((req, res) => {
+    res.status(successStatus).json({message: `Put contact for ${req.param.id}` });
+});
+
+router.route("/:id").patch((req, res) => {
+    res.status(successStatus).json({message: `Update contact for ${req.param.id}` });
+});
+
+router.route("/:id").delete((req, res)=> {
+    res.status(successStatus).json({message: `Delete contact for ${req.param.id}` });
+});
+
 
 module.exports = router;
