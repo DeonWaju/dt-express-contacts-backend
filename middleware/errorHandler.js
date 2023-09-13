08c595fc7b4ae1e2a2bd88129ac1f8f729be5405
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-09-13 13:59:54 +0100

    completed errorHandler.js file

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,22 +1,43 @@
-
+const {constants} = require("../constants");
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
 
     switch (statusCode) {
-        case 400:
+        case constants.VALIDATION_ERROR:
             res.json({
                 title: "Validation Failed",
                 message: err.message,
                 stackTrace: err.stack
             });
             break;
-        case 404:
+        case constants.AUTHORIZATION_ERROR:
+            res.json({
+                title: "Authorization Failed",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+        case constants.FORBIDDEN_ERROR:
+            res.json({
+                title: "Forbidden",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+        case constants.NOT_FOUND_ERROR:
             res.json({
                 title: "Not found", 
                 message: err.message, 
                 stackTrace: err.stack
             });
+        case constants.SERVER_ERROR:
+            res.json({
+                title: "Server error", 
+                message: err.message, 
+                stackTrace: err.stack
+            });
             default:
+                console.log("Successful");
             break;
     }
 };
