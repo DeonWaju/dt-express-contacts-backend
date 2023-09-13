867f83cb95d47e161283519c18781969c3ef60af
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-09-13 20:58:50 +0100

    connected mongodb, installed mongoose, created config folder

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectionDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+connectionDb()
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactsRoutes"));
